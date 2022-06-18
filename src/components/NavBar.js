86e729907612373b22a5722faa3f17849ba3ba2d
Márcia Martins <marciammart@gmail.com>
2022-06-18 09:30:37 +0100

    Add tablet version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import logoDark from "../assets/logo-dark.png";
+import FeatherIcon from "feather-icons-react";
+import { useOnClickOutside } from "use-hooks";
 
 const NavBar = () => {
+  const ref = useRef();
+  const [open, setOpen] = useState(false);
+
+  useOnClickOutside(ref, () => setOpen(false));
+
   return (
     <div className="navbar">
       <img className="navbar-logo" src={logoDark} alt="metropolitana" />
-      <div className='navbar-menu'>
-        <div className='navbar-link'>Sobre</div>
-        <div className='navbar-link'>Contactar</div>
+      <div ref={ref}>
+        <div className="navbar-menu-mobile" onClick={() => setOpen(!open)}>
+          <FeatherIcon icon="menu" size="25" />
+        </div>
+        <div className={`navbar-menu ${open && "navbar-menu-open"}`}>
+          <div className="navbar-link">Sobre</div>
+          <div className="navbar-link">Contactar</div>
+        </div>
       </div>
-    </div>
+    </div >
   );
 };
 
